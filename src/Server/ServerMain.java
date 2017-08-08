b98d9c1d3821dfcf7fce2a8d474e8424e0cf2f0a
Harry-Gaskin <harryrhysgaskin@gmail.com>
2017-08-08 18:03:16 +0100

    Created ServerMain class that can handle multiple clients with threads.

---
+++ src/Server/ServerMain.java
@@ -0,0 +1,60 @@
+package Server;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ * Server for the messaging service.
+ * Server uses threads to handle multiple simultaneous clients.
+ *
+ * Server must be provided a port number via args and must be set up before any clients.
+ *
+ * @author      Harry Gaskin
+ * @version     %I%, %G%
+ */
+public class ServerMain {
+    public static void main(String args[]) {
+        if(args.length != 1) {
+            System.err.println("Server.ServerMain requires command line argument <port number>");
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        try(
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                //BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+                ) {
+            boolean allowingConnections = true;
+
+            while(allowingConnections)
+                (new Thread(new ClientHandlerThread(serverSocket.accept()))).start();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class ClientHandlerThread implements Runnable {
+
+        private Socket socket;
+
+        public ClientHandlerThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (
+                    PrintWriter out = new PrintWriter(socket.getOutputStream());
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+            ) {
+                if(in.readLine().equalsIgnoreCase("Hello Server")) {
+                    out.println("Hello Client");
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
